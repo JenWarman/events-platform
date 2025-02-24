e351ad1078955dee6e428580c261b327d478d200
Jen Warman <jen.warman85@gmail.com>
2025-02-24 15:05:14 +0000

    Search component css.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { EventSearchComponent } from "./event-search/event-search.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, EventSearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/event-search/event-search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-search',
+  imports: [],
+  templateUrl: './event-search.component.html',
+  styleUrl: './event-search.component.css'
+})
+export class EventSearchComponent {
+
+}
