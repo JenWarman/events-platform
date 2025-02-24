c02c88294c8c7367e12e5f581b900d25a667a862
Jen Warman <jen.warman85@gmail.com>
2025-02-24 14:33:35 +0000

    UserEvents and routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
+import { UserEventsComponent } from './user-events/user-events.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent
-    },
-    {
-        path: 'profile',
-        component: ProfileComponent,
-    }
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+  {
+    path: 'your-events',
+    component: UserEventsComponent,
+  },
 ];
---
+++ src/app/user-events/user-events.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-events',
+  imports: [],
+  templateUrl: './user-events.component.html',
+  styleUrl: './user-events.component.css'
+})
+export class UserEventsComponent {
+
+}
