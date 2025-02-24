d179b16346abb946d84ec05e6f7a6528b2c9afd7
Jen Warman <jen.warman85@gmail.com>
2025-02-24 15:26:33 +0000

    Event list component.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { EventListComponent } from "../event-list/event-list.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  imports: [EventListComponent]
 })
 export class HomeComponent {
 
