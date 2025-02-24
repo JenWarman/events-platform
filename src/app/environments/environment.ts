f17b3bc7f6586b8cb4ae08f81220c47f58084483
Jen Warman <jen.warman85@gmail.com>
2025-02-24 14:08:38 +0000

    Header and Home component.

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://wgwlktajnqgkydwyzxxd.supabase.co',
+    supabaseKey:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indnd2xrdGFqbnFna3lkd3l6eHhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk4NzgyNjMsImV4cCI6MjA1NTQ1NDI2M30.OKbXQazrf9x8hLWD74tNsjfLVttQWyGFHQZr-7prGFE',
+  };
