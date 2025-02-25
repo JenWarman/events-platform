2425cac2a9639438566dc9ae9af9c42ebf9ed476
Jen Warman <jen.warman85@gmail.com>
2025-02-25 14:00:41 +0000

    Hero component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { EventSearchComponent } from "./event-search/event-search.component";
+import { SupabaseService, UserProfile } from './services/supabase.service';
+import { HeroBarComponent } from './hero-bar/hero-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, EventSearchComponent],
+  imports: [RouterOutlet, HeaderComponent, EventSearchComponent, HeroBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'events-platform';
+  user: UserProfile | null = null;
+
+  constructor(private supabaseService: SupabaseService, private routerService: Router) {
+    this.supabaseService.userLoaded.subscribe((user) => {
+      this.user = user ?? null;
+    });
+  }
 }
---
+++ src/app/hero-bar/hero-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-bar',
+  imports: [],
+  templateUrl: './hero-bar.component.html',
+  styleUrl: './hero-bar.component.css'
+})
+export class HeroBarComponent {
+
+}
