1cd7dcbd7596433418518a15c667572243e9791b
Jen Warman <jen.warman85@gmail.com>
2025-02-26 13:57:51 +0000

    Fetch and list events.

--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SupabaseService } from '../services/supabase.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-event-list',
@@ -6,6 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './event-list.component.html',
   styleUrl: './event-list.component.css'
 })
-export class EventListComponent {
+export class EventListComponent implements OnInit {
+  events: any;
 
+  constructor(
+    private supabaseService: SupabaseService,
+    private routerService: Router
+  ) {}
+
+  onOpenEventDetails(id: string) {
+    this.routerService.navigateByUrl(`/event-item/${id}`);
+  }
+
+  ngOnInit(): void {
+    this.supabaseService.fetchEvents().then((events) => {
+      this.events = events;
+    });
+  }
 }
