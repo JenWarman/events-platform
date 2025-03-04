a13758ee2aeb6ad07eac89ca4043c239e2dc3c64
Jen Warman <jen.warman85@gmail.com>
2025-03-04 17:16:30 +0000

    work on profile component.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { SupabaseService } from '../services/supabase.service';
+import { UserEventsComponent } from '../user-events/user-events.component';
+import { User } from '@supabase/supabase-js';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [UserEventsComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  user : User | null = null;
 
+  constructor(private supabaseService: SupabaseService){
+    this.supabaseService.userLoaded.subscribe((user) => {
+      this.user = user ?? null;})
+  }
+  
 }
