f8d0464b7a77f43e3aaf77ef05cd52fde289663a
Jen Warman <jen.warman85@gmail.com>
2025-03-06 14:21:37 +0000

    register validation.

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,24 +1,47 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { SupabaseService } from '../../services/supabase.service';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  formData = {
-      email: '',
-      password: '',
-    };
+  form = new FormGroup({
+     email: new FormControl('', {
+       validators: [Validators.email, Validators.required]
+     }),
+     password: new FormControl('', {
+       validators: [Validators.required, Validators.minLength(8)]
+     })
+   })
   
     constructor(private supabaseService: SupabaseService, private routerService: Router) {}
   
+    get emailIsInvalid() {
+      return (
+        this.form.controls.email.touched &&
+        this.form.controls.email.dirty &&
+        this.form.controls.email.invalid
+      );
+    }
+  
+    get passwordIsInvalid() {
+      return (
+        this.form.controls.password.touched &&
+        this.form.controls.password.dirty &&
+        this.form.controls.password.invalid
+      );
+    }
+    
     onSubmit() {
-      this.supabaseService.registerUser(this.formData)
+      if (!this.form.value.email || !this.form.value.password ) {
+        return;
+      }
+      this.supabaseService.registerUser({email: this.form.value.email, password: this.form.value.password})
         .then((response) => {
           console.log('success')
           this.routerService.navigateByUrl('/')
