e1df4aa369e9798d2ed960bf3b2afb5aa4b67c40
Jen Warman <jen.warman85@gmail.com>
2025-03-12 14:49:40 +0000

    Footer CSS

--- src/app/event-card/event-card.component.ts
+++ src/app/event-card/event-card.component.ts
@@ -59,67 +59,67 @@ export class EventCardComponent implements OnInit {
     this.isPopupVisible = false;
   }
 
-  async initClient() {
-    await gapi.client.init({
-      apiKey: 'AIzaSyCs8kF78b9-aq7Ot5VoTx1Hu9MTfxsF2ho',
-      discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
-    });
-    this.gapiInited = true;
-  }
+  // async initClient() {
+  //   await gapi.client.init({
+  //     apiKey: 'AIzaSyCs8kF78b9-aq7Ot5VoTx1Hu9MTfxsF2ho',
+  //     discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
+  //   });
+  //   this.gapiInited = true;
+  // }
 
-  gisLoaded() {
-    console.log('gisLoaded')
-    this.tokenClient = gapi.oauth2.initTokenClient({
-      client_id: '777277188325-k49mb0ja0ms5kg4cu2shsmeufhmlkbv9.apps.googleusercontent.com',
-      scope: 'https://www.googleapis.com/auth/calendar.readonly',
-      callback: '', // defined later
-    });
-    this.gisInited = true;
-  }
+  // gisLoaded() {
+  //   console.log('gisLoaded')
+  //   this.tokenClient = gapi.oauth2.initTokenClient({
+  //     client_id: '777277188325-k49mb0ja0ms5kg4cu2shsmeufhmlkbv9.apps.googleusercontent.com',
+  //     scope: 'https://www.googleapis.com/auth/calendar.readonly',
+  //     callback: '', // defined later
+  //   });
+  //   this.gisInited = true;
+  // }
 
 
-  createGoogleEvent(eventDetails: any) {
-    console.log('is createGoogleEvent working?')
-    this.tokenClient.callback = async (resp: any) => {
-      if (resp.error !== undefined) {
-        throw resp;
-      }
-      await this.scheduleEvent(eventDetails);
-    };
-    if (gapi.client.getToken() === null) {
-      this.tokenClient.requestAccessToken({ prompt: "consent" });
-    } else {
-      this.tokenClient.requestAccessToken({ prompt: "" });
-    }
-  }
+  // createGoogleEvent(eventDetails: any) {
+  //   console.log('is createGoogleEvent working?')
+  //   this.tokenClient.callback = async (resp: any) => {
+  //     if (resp.error !== undefined) {
+  //       throw resp;
+  //     }
+  //     await this.scheduleEvent(eventDetails);
+  //   };
+  //   if (gapi.client.getToken() === null) {
+  //     this.tokenClient.requestAccessToken({ prompt: "consent" });
+  //   } else {
+  //     this.tokenClient.requestAccessToken({ prompt: "" });
+  //   }
+  // }
 
-  scheduleEvent(eventDetails: any) {
-    let request
-    let eventToAdd = {
-      title: this.event.title,
-      summary:this.event.summary,
-      location: this.event.location,
-      date: this.event.date,
-      time: this.event.time
-    }
-    request = gapi.client.calendar.events.insert({
-      calendarId: "primary",
-      resource: eventToAdd,
-    })
-    request.execute(function (eventToAdd: any) {
-      console.info("Event created: " + eventToAdd.htmlLink);
-    });
-  }
+  // scheduleEvent(eventDetails: any) {
+  //   let request
+  //   let eventToAdd = {
+  //     title: this.event.title,
+  //     summary:this.event.summary,
+  //     location: this.event.location,
+  //     date: this.event.date,
+  //     time: this.event.time
+  //   }
+  //   request = gapi.client.calendar.events.insert({
+  //     calendarId: "primary",
+  //     resource: eventToAdd,
+  //   })
+  //   request.execute(function (eventToAdd: any) {
+  //     console.info("Event created: " + eventToAdd.htmlLink);
+  //   });
+  // }
 
-  loadGapi(): Promise<void> {
-    const script = document.createElement('script');
-    script.src = 'https://apis.google.com/js/api.js';
-    window.document.body.appendChild(script);
-    return new Promise<void>((resolve, reject) => {
-      script.addEventListener('error', (error) => reject(error));
-      script.addEventListener('load', () => resolve());
-    });
-  }
+  // loadGapi(): Promise<void> {
+  //   const script = document.createElement('script');
+  //   script.src = 'https://apis.google.com/js/api.js';
+  //   window.document.body.appendChild(script);
+  //   return new Promise<void>((resolve, reject) => {
+  //     script.addEventListener('error', (error) => reject(error));
+  //     script.addEventListener('load', () => resolve());
+  //   });
+  // }
 
   async ngOnInit() {
     if (!this.id) {
@@ -128,7 +128,7 @@ export class EventCardComponent implements OnInit {
     this.supabaseService.fetchEventById(this.id).then((event) => {
       this.event = event;
     });
-    await this.loadGapi();
-    gapi.load('client', this.initClient.bind(this));
+    // await this.loadGapi();
+    // gapi.load('client', this.initClient.bind(this));
   }
 }
